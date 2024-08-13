fca953f96ed8206b2febf8b9fa65bea63b7216cf
muhammadshah0815 <muhammadshah0815@gmail.com>
2024-08-14 00:41:32 +0500

    test

---
+++ pracleetcode.java
@@ -0,0 +1,13 @@
+class pracleetcode {
+    public int removeDuplicates(int[] nums) {
+        int[] result = new int[nums.length];
+        int j = 0;
+        int duplicates = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] in ){
+                nums[i]=0;
+            }
+        }
+        return j;
+    }
+}
